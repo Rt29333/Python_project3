2b95b6bb03c2c59116646badbea95b9aa3d65cbc
Rt29333 <2933364109@qq.com>
2022-05-17 21:33:17 +0800

    第六次更改，成功实现选取一个人的功能

--- Select1.py
+++ Select1.py
@@ -20,27 +20,42 @@ class Random_Slect(QtWidgets.QMainWindow, Ui_MainWindow):
         self.lineEdit.setText('会是谁呢？')
         self.lineEdit.setReadOnly(True)
 
+        self.pushButton_4.clicked.connect(self.Click_Clear)
+        self.pushButton_4.setEnabled(False)
+
         self.pushButton.clicked.connect(self.start)
         self.pushButton.setEnabled(False)
 
         self.pushButton_2.clicked.connect(self.stop)
+        # 默认禁用暂停按键
         self.pushButton_2.setEnabled(False)
 
         self.pushButton_3.clicked.connect(self.Click_Insert)
 
+
+        # 默认选择 随机点名
         self.radioButton_2.setChecked(True)
+
+
+    def Click_Clear(self):
+        self.lineEdit.clear()
+        self.lineEdit.setText('会是谁呢？')
+        self.pushButton.setEnabled(False)
+        self.pushButton_3.setEnabled(True)
+        self.pushButton_4.setEnabled(False)
 
     def Click_Insert(self):
         try:
             self.Insert_path, type = QtWidgets.QFileDialog.getOpenFileName(None, '打开文件',
                                                                            'D:/python Projects/Python project3',
                                                                            'Type files(*.txt)')
-            print(self.Insert_path)
+            # print(self.Insert_path)
             if self.Insert_path != "":
                 self.pushButton.setEnabled(True)
+                self.pushButton_3.setEnabled(False)
 
         except Exception as e:
-            print(e)
+            # print(e)
             QtWidgets.QMessageBox.warning(self.widget, '提示', '未导出学生信息名单')
 
     def Click_Show(self):
@@ -48,7 +63,7 @@ class Random_Slect(QtWidgets.QMainWindow, Ui_MainWindow):
             with open(self.Insert_path, 'r', encoding='utf8') as fp:
                 name_read = fp.read()
             name_list = name_read.split('\n')
-            print(name_list)
+            # print(name_list)
             if self.radioButton.isChecked():
                 for name in name_list:
                     # print('开始111', self.num)
@@ -56,22 +71,22 @@ class Random_Slect(QtWidgets.QMainWindow, Ui_MainWindow):
                     if self.num == self.num1 - 1:
                         break
                     # 实时刷新,随机显示
-                    print(name)
+                    # print(name)
                     QtWidgets.QApplication.processEvents()
                     self.lineEdit.setText(name)
             elif self.radioButton_2.isChecked():
                 numbers = random.sample(range(0, len(name_list)), len(name_list))
-                print(numbers)
+                # print(numbers)
                 # print("随机点名")
                 for num in numbers:
                     name = name_list[num]
-                    print(name)
+                    # print(name)
                     if self.num == self.num1 - 1:
                         break
                     QtWidgets.QApplication.processEvents()
                     self.lineEdit.setText(name)
         except Exception as e:
-            print(e)
+            # print(e)
             QtWidgets.QMessageBox.warning(self.widget, '提示', '未导出学生信息名单')
 
     def start(self):
@@ -84,14 +99,17 @@ class Random_Slect(QtWidgets.QMainWindow, Ui_MainWindow):
             # print('开始', self.num)
             self.pushButton.setEnabled(False)
             self.pushButton_2.setEnabled(True)
+            self.pushButton_3.setEnabled(False)
+            self.pushButton_4.setEnabled(False)
             self.timer = QTimer(self)
             self.timer.timeout.connect(self.Click_Show)
             self.timer.start()
         except Exception as e:
-            print(e)
+            # print(e)
             QtWidgets.QMessageBox.warning(self.widget, '提示', '未导出学生信息名单')
 
     def stop(self):
+        self.pushButton_4.setEnabled(True)
         self.num1 += 1
         # print('暂停', self.num)
         self.pushButton_2.setEnabled(False)
